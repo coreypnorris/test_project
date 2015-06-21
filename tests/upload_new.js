302cf55701d82f5a67790631a89c285b2cb0f5af
Corey Norris <coreypnorris@gmail.com>
2015-06-21 07:00:13 -0700

    create test for upload_new page

--- tests/upload_new.js
+++ tests/upload_new.js
@@ -1,9 +1,9 @@
 module.exports = {
     'Homepage Text' : function (browser) {
         browser
-            .url('http://localhost:3000')
+            .url('http://admin:111111@localhost:3000/upload')
             .waitForElementVisible('body', 1000)
-            .assert.containsText('body', 'Public Comics')
+            .assert.containsText('body', 'Upload a new comic')
             .end();
     }
 };
