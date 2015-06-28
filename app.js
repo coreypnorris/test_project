3f1c2f678473569d9d38348df800c020c5cd44dc
Corey Norris <coreypnorris@gmail.com>
2015-06-28 13:38:01 -0700

    tidy up code

--- app.js
+++ app.js
@@ -2,31 +2,18 @@
  * Created by corey_000 on 6/6/2015.
  */
 
-var express = require('express');
-var app = express();
-var utils = require('./utilities.js');
-
-// Environment Configurations
 require('dotenv').load();
 
-var sass    = require('node-sass');
-var bourbon = require('node-bourbon');
-sass.render({
-    file: './application.scss',
-    success: function(css){
-        console.log(css);
-    },
-    error: function(error) {
-        console.log(error);
-    },
-    includePaths: bourbon.with('./public/scss_files/custom_styles.scss')
-});
+var express = require('express');
+var app = express();
+var utilities = require('./utilities.js');
+
+// Environment Configurations
 
 var path = require('path');
 var expressLayouts = require('express-ejs-layouts');
 app.set('view engine', 'ejs');
 app.set('layout', 'layout');
-
 app.use(expressLayouts);
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -42,27 +29,13 @@ if ('production' == app.get('env')) {
 }
 
 // Routing
-var basicAuth = require('basic-auth');
-var auth = function (req, res, next) {
-    function unauthorized(res) {
-        res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-        return res.sendStatus(401);
-    }
-    var user = basicAuth(req);
-    if (!user || user.name !== process.env.BASIC_AUTH_USERNAME || user.pass !== process.env.BASIC_AUTH_PASSWORD) {
-        return unauthorized(res);
-    } else {
-        next();
-    }
-};
-
 home = require('./routes/home.js');
 upload = require('./routes/upload.js');
 app.get('/', function(req, res) {
     res.redirect('/home');
 });
 app.get('/home', home.index);
-app.get('/upload', auth, upload.new);
+app.get('/upload', utilities.basicAuth, upload.new);
 
 // Port
 app.listen(3000);
--- utilities.js
+++ utilities.js
@@ -26,3 +26,30 @@
 //        res.send(upload.new);
 //    };
 //};
+
+var sass    = require('node-sass');
+var bourbon = require('node-bourbon');
+sass.render({
+    file: './application.scss',
+    success: function(css){
+        console.log(css);
+    },
+    error: function(error) {
+        console.log(error);
+    },
+    includePaths: bourbon.with('./public/scss_files/custom_styles.scss')
+});
+
+var basicAuth = require('basic-auth');
+exports.basicAuth = function (req, res, next) {
+    function unauthorized(res) {
+        res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+        return res.sendStatus(401);
+    }
+    var user = basicAuth(req);
+    if (!user || user.name !== process.env.BASIC_AUTH_USERNAME || user.pass !== process.env.BASIC_AUTH_PASSWORD) {
+        return unauthorized(res);
+    } else {
+        next();
+    }
+};
